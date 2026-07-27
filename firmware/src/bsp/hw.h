2edc00282db160c63a8bcd979bc4457b8732fd50
DESKTOP-NU36G62\WANG <wang8763@naver.com>
2026-07-27 23:52:00 +0900

    최초 업로드

---
+++ firmware/src/bsp/hw.h
@@ -0,0 +1,31 @@
+/*
+ * hw.h
+ *
+ *  Created on: Dec 6, 2020
+ *      Author: baram
+ */
+
+#ifndef SRC_HW_HW_H_
+#define SRC_HW_HW_H_
+
+#include "hw_def.h"
+#include "def.h"
+#include "bsp.h"
+#include "gpio.h"
+//#include "tim.h"
+//#include "can.h"
+#include "uart.h"
+#include "i2c.h"
+//#include "lcd.h"
+#include "maths.h"
+#include "macros.h"
+#include "cli.h"
+#include "cli_gui.h"
+#include "log.h"
+
+
+
+void hwInit(void);
+
+
+#endif /* SRC_HW_HW_H_ */
